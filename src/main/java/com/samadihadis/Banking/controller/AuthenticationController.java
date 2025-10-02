2fa6881cc6bb4ea682b049e4dc1c5f3e2bf83141
samadihadis <samadi.hadis@gmail.com>
2025-10-02 12:30:43 +0330

    Add AuthenticationController

---
+++ src/main/java/com/samadihadis/Banking/controller/AuthenticationController.java
@@ -0,0 +1,46 @@
+package com.samadihadis.Banking.controller;
+
+import com.samadihadis.Banking.businessLogic.AuthenticationService;
+import com.samadihadis.Banking.dto.request.AuthenticationResponse;
+import com.samadihadis.Banking.dto.request.LoginRequest;
+import com.samadihadis.Banking.dto.request.RegisterRequest;
+import com.samadihadis.Banking.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/authentications")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody @Validated RegisterRequest registerRequest) {
+        try {
+            var registerResponse = authenticationService.register(registerRequest);
+            return ResponseEntity.ok(registerResponse);
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody @Validated LoginRequest loginRequest) {
+        try {
+            var registerResponse = authenticationService.login(loginRequest);
+            return ResponseEntity.ok(registerResponse);
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
+
